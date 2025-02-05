00ad9d43f4a1ebced1adf4c09440591f5532dc5b
Dresden Kershaw <dkershaw@usc.edu>
2025-02-05 13:10:41 -0800

    Reworked class ULListStr to use string arrays instead of strings

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -2,13 +2,76 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 
 #include "ulliststr.h"
 
 //Use this file to test your ulliststr implementation before running the test suite
+void printItem(ULListStr*&);
+void printList(ULListStr*&);
+void printLine(int);
+void debugPrint(std::string);
+void initializeFromFile(std::string, ULListStr*&);
+bool debugMode;
 
 int main(int argc, char* argv[])
 {
+    if(argc < 2) debugMode = true;
 
+    ULListStr* a = new ULListStr();
+    initializeFromFile("ulliststr", a); 
+    printList(a);
+    a->pop_front();
+    printList(a);
+    a->push_front("barnacle");
+    printList(a);
+    a->pop_back();
+    printList(a);
     return 0;
+}
+
+void initializeFromFile(std::string name, ULListStr*& ulist)
+{
+    std::ifstream ifs;
+    ifs.open("selftest_" + name + ".txt");
+    debugPrint("Reading from file: selftest_" + name + ".txt...");
+
+    std::string temp;
+
+    while(ifs.good())
+    {
+        std::getline(ifs,temp);
+        ulist->push_back(temp);
+    }
+    ifs.close();
+
+    debugPrint("Initialized ulliststr from file.");
+}
+
+void debugPrint(std::string message)
+{
+    if(debugMode) 
+    {
+        if(message == "line")
+        {
+            printLine(40);
+        }
+        else std::cout << message << std::endl;
+    }
+}
+
+void printLine(int width)
+{
+    for(int i = 0; i < width; i++)
+    {
+        std::cout << "_";
+    }
+    std::cout << std::endl;
+}
+
+void printList(ULListStr*& ulist)
+{
+    if(!debugMode) return;
+    debugPrint((std::string) ulist->front());
+    debugPrint(ulist->back());
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -26,41 +26,40 @@ size_t ULListStr::size() const
 
 // WRITE YOUR CODE HERE
 //#####################
+//#####################
+//#####################
 
 // Adds a new value to the back of the list
 // O(1) req'd
 void ULListStr::push_back(const std::string& val)
 {
   // goal: find rearmost element+1, insert there
+  // null protection
+  if(empty())
+  {
+    head_ = tail_ = new Item();
+  }
 
   // case 0: has room; just insert
   if(tail_->last < ARRSIZE)
   {
+    tail_->val[tail_->last] = val;
     tail_->last++;
-    tail_->val->push_back(val[0]);
-
-    // case 0a: length of insertion > 1
-    if(val.length() > 1)
-    {
-      std::string temp = val;
-      temp.erase(0);
-      push_back(temp);
-      return;
-    }
   }
   // case 1: no room; allocate new, insert at front
   else
   {
     Item* temp = tail_;
     tail_ = new Item();
+    tail_->prev = temp;
     temp->next = tail_;
+    tail_->next = NULL;
     
-    for(size_t i = 0; i < val.length(); i++)
-    {
-      tail_->val[i] = val[i];
-    }
-    tail_->last = val.length();
+    tail_->val[0] = val;
   }
+
+  // no matter what, update size
+  size_++;
 }
 
 // Removes a value from the back of the list
@@ -68,9 +67,8 @@ void ULListStr::push_back(const std::string& val)
 void ULListStr::pop_back()
 {
   // goal: find rearmost element and trash it
-  
   // case 0: last element in array
-  if(tail_->last == 1)
+  if(tail_->last - tail_->first == 1)
   {
     Item* temp = tail_;
     tail_ = tail_->prev;
@@ -83,38 +81,86 @@ void ULListStr::pop_back()
     tail_->val->erase(tail_->last - 1);
     tail_->last--;
   }
+  
+  // no matter what, update size
+  size_--;
 }
 
 // Adds a new value to the front of the list.
 // O(1) req'd
 void ULListStr::push_front(const std::string& val)
 {
-  // find frontmost element-1
+  // goal: find frontmost element-1
+  // null protection
+  if(empty())
+  {
+    head_ = tail_ = new Item();
+  }
+
   // case 0: has room. just insert
-  // case 1: no room. allocate new, insert at back
+  if(head_->first != 0)
+  {
+    head_->first--;
+    head_->val[head_->first] = val;
+  }
+  // case 1: no room; allocate new
+  else
+  {
+    Item* temp = head_;
+    head_ = new Item();
+    if(temp != NULL) temp->prev = head_;
+    head_->prev = NULL;
+
+    // populate from first to end
+    for(size_t i = ARRSIZE - val.length() - 1; i < head_->first + val.length(); i++)
+    {
+      tail_->val[i] = val[i - head_->first];
+    }
+
+    head_->first = ARRSIZE - val.length();
+    head_->last = ARRSIZE;
+  }
+
+  // no matter what, update size
+  size_++;
 }
 
 // Removes a value from the front of the list
 // O(1) req'd
 void ULListStr::pop_front()
 {
-  // TODO:
+  // goal: find frontmost element and trash it
+  // case 0: entry is "last survivor" in Item
+  if(head_->last - head_->first == 1)
+  {
+    Item* temp = head_;
+    head_ = head_->next;
+    head_->prev = NULL;
+    delete temp;
+  }
+  // case 1: Item has >= 2 entries
+  else
+  {
+    head_->val->erase(head_->first,1);
+    head_->first++;
+  }
+
+  // no matter what, update the size
+  size_--;
 }
 
 // Returns a const reference to the back element
 // O(1) req'd
 std::string const & ULListStr::back() const
 {
-  // TODO: 
-  return NULL;
+  return tail_->val[tail_->last - 1];
 }
 
 // Returns a const reference to the front element
 // O(1) req'd
 std::string const & ULListStr::front() const
 {
-  // TODO:
-  return NULL;
+  return head_->val[head_->first];
 }
 
 // Returns a pointer to the item at index, loc,
@@ -122,10 +168,23 @@ std::string const & ULListStr::front() const
 // O(n) req'd
 std::string* ULListStr::getValAtLoc(size_t loc) const
 {
-  // TODO:
-  return NULL;
+  if(loc > size_) return NULL;
+  if(loc < 0) return NULL;
+
+  Item* item = head_;
+  size_t i = item->val->length();
+
+  while(i < loc)
+  {
+    item = item->next;
+    i += item->val->length();
+  }
+
+  return &item->val[i - loc];
 }
 
+//#####################
+//#####################
 //#####################
 
 void ULListStr::set(size_t loc, const std::string& val)
